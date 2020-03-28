bac732955df400585f14726b17e5563c87be7fb1
Anshul <samgalacestyle01@gmail.com>
2020-03-29 01:42:08 +0530

    Dynamically change style based on date

--- components/MainContent.js
+++ components/MainContent.js
@@ -5,12 +5,19 @@ function MainContent() {
     const hours = date.getHours()
     let timeOfDay
 
+    const styles = {
+        fontSize: 30
+    }
+
     if (hours < 12) {
         timeOfDay = "morning"
+        styles.color = "#ADD8E6" //light-blue
     } else if (hours >= 12 && hours < 17) {
         timeOfDay = "afternoon"
+        styles.color = "#FFFF00" //yellow
     } else {
         timeOfDay = "night"
+        styles.color = "#DA680F" //orange
     }
 
     const firstNane = 'Gildroy'
