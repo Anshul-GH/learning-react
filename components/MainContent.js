20b88f3d730add31428ed12db99e61c3fffd1b0f
Anshul <samgalacestyle01@gmail.com>
2020-03-29 01:20:55 +0530

    JSX and JS code together with dynamic time

--- components/MainContent.js
+++ components/MainContent.js
@@ -1,14 +1,23 @@
 import React from "react"
 
 function MainContent() {
+    const date = new Date()
+    const hours = date.getHours()
+    let timeOfDay
+
+    if (hours < 12) {
+        timeOfDay = "morning"
+    } else if (hours >= 12 && hours < 17) {
+        timeOfDay = "afternoon"
+    } else {
+        timeOfDay = "night"
+    }
+
+    const firstNane = 'Gildroy'
+    const lastName = 'Lockhart'
+
     return (
-        <div>
-            <ul>
-                <li>One</li>
-                <li>Two</li>
-                <li>Three</li>
-            </ul>
-        </div>
+    <h1>Good {timeOfDay}, Mr. {firstNane} {lastName}</h1>
     )
 }
 
